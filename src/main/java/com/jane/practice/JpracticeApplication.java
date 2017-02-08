49626d05424adbd6aef99bc25f69d5f00ea0d0e8
jane <kitty33733@gmail.com>
2017-02-08 16:58:45 +0800

    initial

---
+++ src/main/java/com/jane/practice/JpracticeApplication.java
@@ -0,0 +1,12 @@
+package com.jane.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpracticeApplication.class, args);
+	}
+}
